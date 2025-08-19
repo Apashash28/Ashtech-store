7becc1d1c76ebcdb3b9093c04a11c617830026f2
Apash <apashash3@gmail.com>
2025-08-19 04:39:23 +0100

    Delete includes directory

--- includes/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-// Vérifier si l'utilisateur est connecté
-if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] != true) {
-    header('Location: admin_login.php'); // Rediriger vers la page de connexion si non connecté
-    exit();
-}
